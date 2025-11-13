182d3b893f806b932472c2aa168293c92a60bc81
MFauziik <fauziopoy00@gmail.com>
2025-11-13 10:52:24 +0700

    udh sampe peminjaman

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -6,10 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Inventory extends Model
 {
-     protected $fillable = ['kode_barang', 'nama_barang', 'categories_id', 'status', 'lokasi_barang', 'is_active'];
+     protected $fillable = ['kode_barang', 'nama_barang', 'kategori', 'status', 'lokasi_barang', 'is_active'];
 
-    public function category()
-    {
-        return $this->belongsTo(Category::class, 'categories_id');
-    }
-}
+} 
